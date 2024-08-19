044a60df99a5b7b9861b71d390de55cb4083e013
shakthichaithanya <shakthits01@gmail.com>
2024-08-19 12:46:11 +0530

    supressing warnings and optimized text widget

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,8 +1,13 @@
+// ignore_for_file: avoid_web_libraries_in_flutter
+
+// ignore: unused_import
 import 'dart:html' as html;
 import 'dart:js' as js;
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
   void startScreenSharing() {
     // This calls a JavaScript function to start screen sharing
     js.context.callMethod('startScreenSharing');
@@ -12,12 +17,12 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Web Screen Sharing'),
+        title:const Text('Web Screen Sharing'),
       ),
       body: Center(
         child: ElevatedButton(
           onPressed: startScreenSharing,
-          child: Text('Start Screen Sharing'),
+          child: const Text('Start Screen Sharing'),
         ),
       ),
     );
